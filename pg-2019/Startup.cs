27392d4a6fdbc3072c77eb2366519f3a36f6934e
Navyathallapally <navyathallapally4@gmail.com>
2021-11-16 17:20:27 +0530

    hi pavani

--- pg-2019/Startup.cs
+++ pg-2019/Startup.cs
@@ -33,6 +33,7 @@ namespace pg_2019
                 endpoints.MapGet("/", async context =>
                 {
                     await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsync("hi pavani");
                 });
             });
         }
